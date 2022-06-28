982f81960acbe397ce2f088ac4788dba78a02f42
amberxuemingxin <amberxue@seas.upenn.edu>
2022-06-28 21:16:47 +0000

    delete file with typo

--- redelpy-site.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-tmux kill-server
-cd amber-web
-git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-tmux new -d 'flask run --host=0.0.0.0'
-
