2b7776ae51067c06ed600d84b46eff3cb1c41157
amberxuemingxin <amberxue@seas.upenn.edu>
2022-07-06 18:21:41 +0000

    update redeploy.sh typo

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,7 +3,7 @@
 cd amber-web
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml dup -d --build
+docker compose -f docker-compose.prod.yml up -d --build
 
 # source python3-virtualenv/bin/activate
 # pip install -r requirements.txt
